ae61fe6ad2ccda6d1523d9410cb8f305ee17c81a
danieldouglas <danieldouglas1454@gmail.com>
2025-08-20 21:16:58 -0300

    resumo da alteração

---
+++ AtividadeEmSala/src/AtividadeSala05.java
@@ -0,0 +1,14 @@
+public class AtividadeSala05 {
+    public static void main(String[] args){
+        char letra = 'S';
+        char inicial = 'V';
+        String texto = "code";
+        //Concatenação
+        String temp = "";
+        temp += "" + inicial + letra;
+        System.out.println(temp);
+        System.out.println(temp + texto);
+
+
+    }
+}
