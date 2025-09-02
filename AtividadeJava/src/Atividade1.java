c71568e24bd08e24784eb644e9edf676312d7e1e
danieldouglas <danieldouglas1454@gmail.com>
2025-09-02 20:41:01 -0300

    Atividade 1 e 7 finalizadas

--- AtividadeJava/src/Atividade1.java
+++ AtividadeJava/src/Atividade1.java
@@ -1,2 +1,12 @@
 public class Atividade1 {
+
+    public static void main(String[] args) {
+        String nome = "Daniel";
+        String cidade = "São Gonçalo";
+        final String PAIS = "Brasil";
+
+        System.out.println("Olá, eu sou " + nome + " moro em " +
+                cidade + " /" + PAIS      );
+    }
+
 }
---
+++ AtividadeJava/src/Atividade7.java
@@ -0,0 +1,14 @@
+public class Atividade7 {
+
+    public static void main(String[] args) {
+        // 7. Leia dois inteiros a e b ; calcule soma = a + b e exiba soma .
+
+        int a = 8;
+        int b = 20;
+        int soma = a + b;
+
+        System.out.println("a = " + 8);
+        System.out.println("b = " + 20);
+        System.out.println("A soma de a + b é " + soma);
+    }
+}
