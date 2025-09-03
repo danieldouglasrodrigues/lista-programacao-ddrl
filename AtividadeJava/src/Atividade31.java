18fa42024dc63b68f8863c858811977b4777d0c3
danieldouglas <danieldouglas1454@gmail.com>
2025-09-02 21:53:12 -0300

    Atualização de atividades

---
+++ AtividadeJava/src/Atividade31.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Atividade31 {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int valor = 0, soma = 0;
+        do {
+            System.out.println("Digite o valor:");
+            valor = leia.nextInt();
+        }while (valor != 0);
+        System.out.println("O resultado da soma dos valores é: " + soma);
+    }
+}
---
+++ AtividadeJava/src/Atividade32.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Atividade32 {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        final String senha = "1234";
+
+
+    }
+}
