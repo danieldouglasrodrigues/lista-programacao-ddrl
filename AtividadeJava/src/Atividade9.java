abff76c2ed45f0b6cd6a702da558a0d64f65c545
danieldouglas <danieldouglas1454@gmail.com>
2025-09-03 19:45:06 -0300

    Atualização

--- AtividadeJava/src/Atividade9.java
+++ AtividadeJava/src/Atividade9.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
-public class Atividade09 {
+public class Atividade9 {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
         double base = 0, altura = 0;
         double perimetro = 0;
-        System.out.println("Digite base e altura para saber seu perímetro");
+        System.out.println("Digite base e altura para saber seu perimetro:");
         base = leia.nextDouble();
         altura = leia.nextDouble();
         perimetro = 2 * (base + altura);
-        System.out.println("O perímetro é igual a: " + perimetro);
+        System.out.println("O perimetro é igual a: " + perimetro);
+
     }
 }
